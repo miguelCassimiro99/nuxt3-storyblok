152f75550a9b7fa47e59d0bcdc678ea2c2f769f3
Maiguelman <miguel.cassimiro99@gmail.com>
2023-10-21 23:45:39 -0300

    fix(security): removed storyblok api key from code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   devtools: { enabled: true, timeline: { enabled: true } },
   modules: ['@storyblok/nuxt', '@nuxtjs/tailwindcss'],
 
   storyblok: {
-    accessToken: 'McgtG0TUQWwtsl3oaj4bXAtt',
+    accessToken: `${process.env.STORYBLOK_API_KEY}`,
     apiOptions: {
       region: 'us',
     },
